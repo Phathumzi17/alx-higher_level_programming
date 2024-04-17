75fb5d68a59ec9a5aad8aeacd82ba35ab4930464
Phathumzi <phathumzindlovu@gmail.com>
2024-04-17 15:36:58 -0700

    5

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''Module for attributes_methods function.'''
 
-def attributes_methods(obj):
+def lookup(obj):
     '''Lists attributes and methods of an object.
     Args:
-        ob (object): The object to list.
+        ob(object): The object to list.
 
     Returns:
         list: List of attributes and methods.
